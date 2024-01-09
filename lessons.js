cde05a3c5f7a2084c7627afddfaebb279cd12fb8
Sameer Limbu <sameerlimbu31@gmail.com>
2024-01-09 00:18:06 +0000

    Added lessons file

---
+++ lessons.js
@@ -0,0 +1,85 @@
+const lessons = [
+    {
+        id: 100,
+        title: "Computer Programming",
+        location: "Hendon",
+        price: 500,
+        image: "/public/pic/coding.png",
+        availableInventory: 10
+    },
+    {
+        id: 101,
+        title: "Math",
+        location: "Ashford",
+        price: 650,
+        image: "/public/pic/math.png",
+        availableInventory: 8
+    },
+    {
+        id: 102,
+        title: "Science",
+        location: "Oxford",
+        price: 500,
+        image: "/public/pic/science.png",
+        availableInventory: 6
+    },
+    {
+        id: 103,
+        title: "Astronomy",
+        location: "Portsmouth",
+        price: 850,
+        image: "/public/pic/astronomy.png",
+        availableInventory: 5
+    },
+    {
+        id: 104,
+        title: "Health",
+        location: "Reading",
+        price: 550,
+        image: "public/pic/health.png",
+        availableInventory: 12
+    },
+    {
+        id: 105,
+        title: "Business",
+        location: "Cambridge",
+        price: 200,
+        image: "public/pic/business.png",
+        availableInventory: 10
+    },
+    {
+        id: 106,
+        title: "Accounting",
+        location: "Aldershot",
+        price: 250,
+        image: "public/pic/accounting.png",
+        availableInventory: 11
+    },
+    {
+        id: 107,
+        title: "Sports",
+        location: "Swansea",
+        price: 200,
+        image: "public/pic/sports.png",
+        availableInventory: 7
+    },
+    {
+        id: 108,
+        title: "Engineering",
+        location: "London",
+        price: 400,
+        image: "public/pic/engineering.png",
+        availableInventory: 5
+    },
+    {
+        id: 109,
+        title: "Culinary",
+        location: "Brighton",
+        price: 300,
+        image: "public/pic/culinary.png",
+        availableInventory: 15
+    },
+   
+    
+];
+export default lessons;
